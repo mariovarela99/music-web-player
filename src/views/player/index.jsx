c89d47ea4fdd238f0e1a51d2e9e29611857f42ab
mariovarela99 <mariovarela99@outlook.com>
2022-08-26 23:02:06 +0100

    Add Basic Style - Primary Figure

--- src/views/player/index.jsx
+++ src/views/player/index.jsx
@@ -4,9 +4,15 @@ import "./style.scss";
 function Player() {
   return (
     <main className="wrapper">
-      <div className="transparent-box"></div>
-      <div className="transparent-box"></div>
-      <div className="transparent-box"></div>
+      <div className="transparent-box player">
+        <h1> Now Playing </h1>
+      </div>
+      <div className="transparent-box playlists">
+        <h1> Playlists </h1>
+      </div>
+      <div className="transparent-box music-list">
+        <h1> Music List </h1>
+      </div>
     </main>
   );
 }
